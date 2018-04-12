851a366e7bee4328d8188698288d0deff402cbcd
bartekma89 <bartekma89@gmail.com>
2018-04-12 23:58:11 +0200

    added DevTools

--- src/index.js
+++ src/index.js
@@ -3,15 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import { applyMiddleware, createStore } from 'redux';
-import { createLogger } from 'redux-logger';
+import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import rootReducer from './reducers/';
+import DevTools from './DevTools.js';
 
-const logger = createLogger();
-const store = createStore(rootReducer, applyMiddleware(logger));
-
-//store.subscribe(() => console.log(store.getState()));
+const store = createStore(rootReducer, DevTools.instrument());
 
 ReactDOM.render(
 	<Provider store={store}>
